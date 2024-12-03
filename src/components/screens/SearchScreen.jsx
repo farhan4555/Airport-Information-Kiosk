030d4648c82a7e1c3459f3dcb82deee15ee4f704
a7rie <aminelnasri9@gmail.com>
2024-12-03 14:09:37 -0700

    Add SearchBar to search page

--- src/components/screens/SearchScreen.jsx
+++ src/components/screens/SearchScreen.jsx
@@ -3,6 +3,7 @@ import Row from 'react-bootstrap/Row';
 import LogoButton from '../common/LogoButton';
 import BackButton from '../common/BackButton';
 import SelectorButton from '../common/SelectorButton';
+import Form from 'react-bootstrap/Form';
 
 function ScreenScreen() {
   return (
@@ -17,9 +18,14 @@ function ScreenScreen() {
             What are you looking for?
           </div>
         </Row>
+        <Form>
+          <Form.Group className="mb-3 searchBar" controlId="exampleForm.ControlInput1">
+            <Form.Control type="text" placeholder="Search" />
+          </Form.Group>
+        </Form>
       </div>
 
-      <div className="w-100 px-3 d-flex flex-column align-items-center">
+      <div className="mt-2 w-100 px-3 d-flex flex-column align-items-center">
         <Row className="mb-3 w-100 justify-content-center">
           <SelectorButton text="Terminals" />
           <SelectorButton text="Baggage Claim" />
@@ -36,11 +42,7 @@ function ScreenScreen() {
           <SelectorButton text="Shops" />
           <SelectorButton text="Parking" />
         </Row>
-        <Row className="mb-3 w-100 justify-content-center">
-          <SelectorButton text="Bus Stops" />
-          <SelectorButton text="Help Desk" />
-        </Row>
-        <Row className="mb-3">
+        <Row className="mt-3">
           <LogoButton text="Airport Map" imgPath="" link="map" />
         </Row>
       </div>
