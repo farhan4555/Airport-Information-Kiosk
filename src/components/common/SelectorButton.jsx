0751c4d4356b69e8b147837c336d37a3aa3ae257
a7rie <aminelnasri9@gmail.com>
2024-12-03 10:47:20 -0700

    Add Search Airport page

---
+++ src/components/common/SelectorButton.jsx
@@ -0,0 +1,15 @@
+import {
+  Link
+} from "react-router-dom";
+
+function SelectorButton({ text, link }) {
+  return (
+    <Link style={{ color: "black" }} to={"/" + link}>
+      <button type="button" className="btn btn-light text-nowrap selectorButton">
+        {text}
+      </button>
+    </Link>
+  );
+}
+
+export default SelectorButton;
--- src/components/screens/SearchScreen.jsx
+++ src/components/screens/SearchScreen.jsx
@@ -1,9 +1,52 @@
 import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import LogoButton from '../common/LogoButton';
+import BackButton from '../common/BackButton';
+import SelectorButton from '../common/SelectorButton';
 
 function ScreenScreen() {
   return (
     <Container className="page d-flex flex-column align-items-center">
-      search airport page
+      <div className='d-flex w-100 justify-content-left'>
+        <BackButton />
+      </div>
+
+      {/* Adjust the positioning of the welcome text */}
+      <div className="p-3 flex-grow-0">
+
+        <Row className="mb-3">
+          <div className='genericTitle searchScreenTitle'>
+            What are you looking for?
+          </div>
+        </Row>
+      </div>
+
+      {/* Adjust the positioning of the buttons */}
+      <div className="w-100 px-3 d-flex flex-column align-items-center">
+        <Row className="mb-3 w-100 justify-content-center">
+          <SelectorButton text="Terminals" />
+          <SelectorButton text="Baggage Claim" />
+        </Row>
+        <Row className="mb-3 w-100 justify-content-center">
+          <SelectorButton text="Washrooms" />
+          <SelectorButton text="Connections" />
+        </Row>
+        <Row className="mb-3 w-100 justify-content-center">
+          <SelectorButton text="Parking" />
+          <SelectorButton text="Hotels" />
+        </Row>
+        <Row className="mb-3 w-100 justify-content-center">
+          <SelectorButton text="Shops" />
+          <SelectorButton text="Parking" />
+        </Row>
+        <Row className="mb-3 w-100 justify-content-center">
+          <SelectorButton text="Bus Stops" />
+          <SelectorButton text="Help Desk" />
+        </Row>
+        <Row className="mb-3">
+          <LogoButton text="Airport Map" imgPath="" link="search" />
+        </Row>
+      </div>
     </Container>
 
   );
