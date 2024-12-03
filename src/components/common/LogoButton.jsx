67d91906d958d13938805267128c8fb302951ca5
a7rie <aminelnasri9@gmail.com>
2024-12-03 15:07:39 -0700

    Add icon images to welcome screen buttons

--- src/components/common/LogoButton.jsx
+++ src/components/common/LogoButton.jsx
@@ -5,7 +5,7 @@ import {
 function LogoButton({ text, imgPath, link }) {
   return (
     <Link style={{ color: "black" }} to={"/" + link}>
-      <button type="button" className="btn btn-light text-nowrap">
+      <button type="button" className="btn btn-light text-nowrap logoButton">
         {imgPath && <img src={imgPath} alt="Logo" className="logo-img" />}
         {text}
       </button>
--- src/components/screens/WelcomeScreen.jsx
+++ src/components/screens/WelcomeScreen.jsx
@@ -7,7 +7,7 @@ function WelcomeScreen() {
   return (
     <Container className="page d-flex flex-column align-items-center">
       {/* Adjust the positioning of the welcome text */}
-      <div className="p-5 flex-grow-0">
+      <div className="p-5 flex-grow-0 titleScreenText">
         <Row className="mb-1">
           <div className='welcomeScreenYYCText'>
             YYC
@@ -15,7 +15,7 @@ function WelcomeScreen() {
         </Row>
         <Row className="mb-1">
           <div className='welcomeScreenWelcomeToText'>
-            Welcome 
+            Welcome
           </div>
         </Row>
         <Row className="mb-3">
@@ -26,30 +26,26 @@ function WelcomeScreen() {
       </div>
 
       {/* Adjust the positioning of the buttons */}
-      <div className="w-100 px-3 d-flex flex-column align-items-center">
+      <div className="w-100 px-3 d-flex flex-column align-items-center welcomeButtons">
         <Row className="mb-3">
           <Col md={6} className="d-flex justify-content-center">
-            <LogoButton text="Airport Map" imgPath="" link="map"/>
+            <LogoButton text="Airport Map" imgPath="./src/components/images/flight.JPG" link="map" />
           </Col>
           <Col md={6} className="d-flex justify-content-center">
-            <LogoButton text="Search Airport" imgPath="" link="search" />
+            <LogoButton text="Search Airport" imgPath="./src/components/images/search.JPG" link="search" />
           </Col>
         </Row>
 
         <Row className="mb-3">
-          <Col md={6} className="d-flex justify-content-center">
-            <LogoButton text="Transportation" imgPath="" link="transportation" />
+          <Col md={6}>
+            <LogoButton text="Transportation" imgPath="./src/components/images/taxi.JPG" link="transportation" />
           </Col>
           <Col md={6}>
-            <Row>
-              <Col xs={6} className="d-flex justify-content-center">
-                <LogoButton text="Flights" imgPath="" link="flights" />
-              </Col>
-              <Col xs={6} className="d-flex justify-content-center">
-                <LogoButton text="Wifi" imgPath="" link="wifi" />
-              </Col>
-            </Row>
+            <LogoButton text="Flights" imgPath="./src/components/images/flight.JPG" link="flights" />
           </Col>
+        </Row>
+        <Row className="mb-3">
+          <LogoButton text="Wifi" imgPath="./src/components/images/wifi.JPG" link="wifi" />
         </Row>
       </div>
     </Container>
