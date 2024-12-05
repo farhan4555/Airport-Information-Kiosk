8781e75de086eb002f9cc58d9dd1b394209c677b
a7rie <aminelnasri9@gmail.com>
2024-12-04 23:33:30 -0700

    fix merge issues

--- src/components/screens/WelcomeScreen.jsx
+++ src/components/screens/WelcomeScreen.jsx
@@ -3,6 +3,7 @@ import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
 import LogoButton from '../common/LogoButton';
 import LogoButton2 from '../common/LogoButton2';
+import { useTranslation } from 'react-i18next'; // Import translation hook
 
 function WelcomeScreen() {
   const { t } = useTranslation(); // Use the translation function
@@ -21,18 +22,18 @@ function WelcomeScreen() {
       {/* Adjust the positioning of the buttons */}
       <div className="welcomeButtonsGrid">
         <div className="grid-item">
-          <LogoButton text="Airport Map" imgPath="./src/components/images/map.png" link="map" />
+          <LogoButton text={t("AirportMap")}  imgPath="./src/components/images/map.png" link="map" />
         </div>
         <div className="grid-item">
-        <LogoButton text="Search Airport" imgPath="./src/components/images/search.png" link="search" />
+        <LogoButton text={t("searchAirport")} imgPath="./src/components/images/search.png" link="search" />
         </div>
         <div className="grid-item">
-        <LogoButton text="Transportation" imgPath="./src/components/images/taxi.png" link="transportation" />
+        <LogoButton text={t("transportation")} imgPath="./src/components/images/taxi.png" link="transportation" />
         </div>
         <div className="grid-item">
         <div className = "welcomeButtonsInnerGrid">
-          <LogoButton2 text="Flights" imgPath="./src/components/images/flight.png" link="flights" />
-          <LogoButton2 text="Wifi" imgPath="./src/components/images/wifi.png" link="wifi" />
+          <LogoButton2 text={t("flights")} imgPath="./src/components/images/flight.png" link="flights" />
+          <LogoButton2 text={t("wifi")} imgPath="./src/components/images/wifi.png" link="wifi" />
         </div>
         </div>
       </div>
